924faec5ceda5f72b02176d1f19c66fb3722b646
Chance Croft <66639666+ChanceJCroft@users.noreply.github.com>
2021-01-18 16:49:22 -0800

    Add files via upload
    Further exploration and experimentation of Linked Lists

--- Intro/introToDataStructuresII.js
+++ Intro/introToDataStructuresII.js
@@ -14,7 +14,7 @@ class LinkedList {
         this.tail = null; //Last element in the list
     }
 
-    //create a method to add new objects, containing the value and next node
+    //create a method to add new objects to the end, containing the value and next node
     append(value) {
         const newNode = { value: value, next: null};
 
@@ -27,5 +27,67 @@ class LinkedList {
         if(!this.head) {
             this.head = newNode;
         }
+    }
+
+    //preprend adds to the beginning of the linked list
+    prepend(value) {
+        const newNode = { value: value, next: this.head };
+
+        //this will just make it the new head, but the original object still does exist
+        this.head = newNode;
+        
+        //if there is no existing tail, make this the head AND tail
+        if(!this.tail) {
+            this.tail = newNode;
+        }
+        
+    }
+
+    delete(value) {
+        //check to make sure the list is not empty, if it is end the function
+
+        if(!this.head) {
+            return null;
+        }
+
+        //if the first value is the one to delete, change the first node to be the next node.
+        //Since its a WHILE loop it will keep checking the head, in case you have multiple values that match
+        while(this.head && this.head.value === value) {
+            this.head = this.head.next;
+        }
+
+        let curNode = this.head;
+        //check if there is a following value in the list
+        while(curNode.next) {
+            //if the next value equals the input value, point towards the NEXT value, skipping the one you want to delete
+            if(curNode.next.value === value) {
+                //easy way to view the next property of the next node
+                curNode.next = curNode.next.next;
+            } else {
+                cureNode = curNode.next;
+            }
+            
+            curNode = curNode.next;
+        }
+
+        //update the tail if the tail was removed
+        if(this.tail.value === value) {
+            //change the tail the latest valid curNode
+            this.tail = curNode;
+        }
+    }
+
+
+    //method to easily turn into an array
+    toArray() {
+        const elemens = [];
+
+        let curNode = this.head;
+        while(curNode) {
+            elements.push(curNode);
+            curNode = curNode.next;
+        }
+
+        return elements;
     }
 }
